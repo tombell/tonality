0c850c63b2790765cbd364cc63cbfd462359a7a4
Tom Bell <tomb@tomb.io>
2023-03-09 03:13:33 +0000

    Update ooze config

--- mutation/mutation_test.go
+++ mutation/mutation_test.go
@@ -11,9 +11,11 @@ import (
 func TestMutation(t *testing.T) {
 	ooze.Release(
 		t,
+		ooze.ForceColors(),
 		ooze.WithRepositoryRoot("../"),
 		ooze.WithTestCommand("make test"),
+		ooze.WithMinimumThreshold(0.5),
+		ooze.Parallel(),
 		ooze.IgnoreSourceFiles("^vendor/"),
-		ooze.ForceColors(),
 	)
 }
